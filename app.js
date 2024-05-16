ae0211bec033f1069e2053984b77339fc45bb377
PriyanshiGoyal501 <priyanshigoyal501@gmail.com>
2024-05-16 12:07:31 +0530

    add utils

---
+++ app.js
@@ -0,0 +1,135 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+};
+
+const express = require('express');
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
+const session = require('express-session');
+const passport = require('passport');
+require('./auth');
+
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
+
+// Database connection
+// const dbURL = "mongodb://127.0.0.1:27017/uexam";
+
+// main()
+//   .then(() => {
+//     console.log("mongodb connection sucessfull");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// async function main() {
+//   await mongoose.connect(dbURL);
+// }
+
+// Set and use
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+// Session
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get('/auth/google',
+passport.authenticate('google', { scope: [ 'email', 'profile' ] }
+));
+
+app.get('/auth/google/callback',
+passport.authenticate( 'google', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/auth/google/failure'
+  })
+);
+
+app.get('/dashboard', isLoggedIn, (req, res) => {
+  res.render("dashboard/index.ejs",
+  {
+    title: "Dashboard - UExam",
+    name: req.user.displayName,
+    profilePicture: req.user.picture,
+  });
+});
+
+app.get('/dashboard/ongoing', isLoggedIn, (req, res) => {
+  res.render("dashboard/pages/ongoing.ejs",
+  {
+    title: "Ongoing - UExam",
+    name: req.user.displayName,
+    profilePicture: req.user.picture,
+  });
+});
+
+app.get('/dashboard/completed', isLoggedIn, (req, res) => {
+  res.render("dashboard/pages/completed.ejs",
+  {
+    title: "Completed - UExam",
+    name: req.user.displayName,
+    profilePicture: req.user.picture,
+  });
+});
+
+app.get('/dashboard/upcoming', isLoggedIn, (req, res) => {
+  res.render("dashboard/pages/upcoming.ejs",
+  {
+    title: "Upcoming - UExam",
+    name: req.user.displayName,
+    profilePicture: req.user.picture,
+  });
+});
+
+app.get('/test', (req, res) => {
+  res.render("exam/instructions.ejs");
+});
+
+app.get('/test/attempt', (req, res) => {
+  res.render("exam/test.ejs");
+});
+
+app.get('/dashboard/profile', isLoggedIn, (req, res) => {
+  res.render("dashboard/profile.ejs",
+  {
+    title: "Profile - UExam",
+    css: "/css/profile.css",
+    id: req.user.id,
+    firstName: req.user.name.givenName,
+    lastName: req.user.name.familyName,
+    name: req.user.displayName,
+    profilePicture: req.user.picture,
+    email: req.user.emails[0].value
+  });
+});
+
+app.get('/logout', function(req, res) {
+  req.logout(function(err) {
+    if (err) {
+      console.log('Logout error:', err);
+            res.status(500).send('Logout failed');
+        } else {
+            req.session.destroy();
+            res.redirect('/');
+        }
+    });
+});
+
+app.get('/auth/google/failure', (req, res) => {
+  res.send('Failed to authenticate..');
+});
+
+app.listen(8080, () => console.log('listening on port: 8080'));
---
+++ auth.js
@@ -0,0 +1,27 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+const CLIENTID = "322533522504-fvjeefbu8nisf9pk72m81r1sr7qjfskh.apps.googleusercontent.com";
+const CLIENTSECRET = "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";
+
+passport.use(new GoogleStrategy({
+    clientID: CLIENTID,
+    clientSecret: CLIENTSECRET,
+    // clientID: process.env.CLIENTID,
+    // clientSecret: process.env.CLIENTSECRET,
+    // callbackURL: "http://localhost:8080/auth/google/callback",
+    callbackURL: "/auth/google/callback",
+    passReqToCallback: true
+},
+    function (request, accessToken, refreshToken, profile, done) {
+            return done(null, profile);
+    }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
